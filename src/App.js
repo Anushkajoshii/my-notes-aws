ee9bc070b88c27eb85bdbe65a3208054d8c98ef6
Anushka Joshi <anushkajoshi020503@gmail.com>
2024-07-26 17:55:01 +0530

    anushka's notesApp

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import { useEffect, useState } from 'react';
 import { generateClient } from 'aws-amplify/api';
-import { createTodo, updateTodo, deleteTodo } from './graphql/mutations';
+import { createTodo } from './graphql/mutations';
 import { listTodos } from './graphql/queries';
 
-const initialState = { id: '', name: '', description: '' };
+import '@aws-amplify/ui-react/styles.css';
+import { withAuthenticator, Button, View } from '@aws-amplify/ui-react';
+
+const initialState = { name: '', description: '' };
 const client = generateClient();
 
-const App = () => {
+const App = ({ signOut }) => {
   const [formState, setFormState] = useState(initialState);
   const [todos, setTodos] = useState([]);
-  const [isEditing, setIsEditing] = useState(false);
 
   useEffect(() => {
     fetchTodos();
@@ -27,7 +29,7 @@ const App = () => {
       const todos = todoData.data.listTodos.items;
       setTodos(todos);
     } catch (err) {
-      console.log('error fetching todos', err);
+      console.log('error fetching todos');
     }
   }
 
@@ -39,74 +41,43 @@ const App = () => {
       setFormState(initialState);
       await client.graphql({
         query: createTodo,
-        variables: { input: todo }
+        variables: {
+          input: todo
+        }
       });
-      fetchTodos();
     } catch (err) {
       console.log('error creating todo:', err);
     }
   }
 
-  async function editTodo() {
-    try {
-      if (!formState.name || !formState.description) return;
-      const todo = { ...formState };
-      await client.graphql({
-        query: updateTodo,
-        variables: { input: todo }
-      });
-      setFormState(initialState);
-      setIsEditing(false);
-      fetchTodos();
-    } catch (err) {
-      console.log('error updating todo:', err);
-    }
-  }
-
-  async function removeTodo(id) {
-    try {
-      await client.graphql({
-        query: deleteTodo,
-        variables: { input: { id } }
-      });
-      fetchTodos();
-    } catch (err) {
-      console.log('error deleting todo:', err);
-    }
-  }
-
-  function startEdit(todo) {
-    setFormState(todo);
-    setIsEditing(true);
-  }
-
   return (
-    <div style={styles.container}>
-      <h2>Amplify Todos</h2>
-      <input
-        onChange={(event) => setInput('name', event.target.value)}
-        style={styles.input}
-        value={formState.name}
-        placeholder="Name"
-      />
-      <input
-        onChange={(event) => setInput('description', event.target.value)}
-        style={styles.input}
-        value={formState.description}
-        placeholder="Description"
-      />
-      <button style={styles.button} onClick={isEditing ? editTodo : addTodo}>
-        {isEditing ? 'Update Todo' : 'Create Todo'}
-      </button>
-      {todos.map((todo, index) => (
-        <div key={todo.id ? todo.id : index} style={styles.todo}>
-          <p style={styles.todoName}>{todo.name}</p>
-          <p style={styles.todoDescription}>{todo.description}</p>
-          <button style={styles.editButton} onClick={() => startEdit(todo)}>Edit</button>
-          <button style={styles.deleteButton} onClick={() => removeTodo(todo.id)}>Delete</button>
-        </div>
-      ))}
-    </div>
+    <View className='App'>
+      <div style={styles.container}>
+        <h2>Anushka's NotesApp</h2>
+        <input
+          onChange={(event) => setInput('name', event.target.value)}
+          style={styles.input}
+          value={formState.name}
+          placeholder="Title"
+        />
+        <input
+          onChange={(event) => setInput('description', event.target.value)}
+          style={styles.input}
+          value={formState.description}
+          placeholder="Description"
+        />
+        <button style={styles.button} onClick={addTodo}>
+          Create 
+        </button>
+        {todos.map((todo, index) => (
+          <div key={todo.id ? todo.id : index} style={styles.todo}>
+            <p style={styles.todoName}>{todo.name}</p>
+            <p style={styles.todoDescription}>{todo.description}</p>
+          </div>
+        ))}
+      </div>
+      <Button style={styles.signOutButton} onClick={signOut}>Sign Out</Button>
+    </View>
   );
 };
 
@@ -117,7 +88,8 @@ const styles = {
     display: 'flex',
     flexDirection: 'column',
     justifyContent: 'center',
-    padding: 20
+    padding: 20,
+    position: 'relative'
   },
   todo: { marginBottom: 15 },
   input: {
@@ -128,30 +100,25 @@ const styles = {
     fontSize: 18
   },
   todoName: { fontSize: 20, fontWeight: 'bold' },
-  todoDescription: { marginBottom: 0 },
+  todoDescription: {
+    marginBottom: 0,
+    whiteSpace: 'pre-wrap', // allows text to wrap based on line breaks and spaces
+    wordWrap: 'break-word', // ensures words break within the container
+  },
   button: {
-    backgroundColor: 'black',
+    backgroundColor: '#008080',
     color: 'white',
     outline: 'none',
     fontSize: 18,
-    padding: '12px 0px',
-    marginBottom: 10
+    padding: '12px 0px'
   },
-  editButton: {
-    backgroundColor: 'blue',
-    color: 'white',
-    outline: 'none',
-    fontSize: 14,
-    padding: '8px 10px',
-    marginRight: 10
-  },
-  deleteButton: {
+  signOutButton: {
+    position: 'absolute',
+    bottom: 700,
+    right: 20,
     backgroundColor: 'red',
-    color: 'white',
-    outline: 'none',
-    fontSize: 14,
-    padding: '8px 10px'
+    color: 'white'
   }
 };
 
-export default App;
+export default withAuthenticator(App);
